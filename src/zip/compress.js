9a90ffdb6c7ec4082f59b9bf755dc7d9a17f460d
mesmeri <angels_ua@ukr.net>
2022-06-04 21:51:17 +0300

    feat: add logic for compressing and decompressing

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,15 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+
 export const compress = async () => {
-    // Write your code here 
+  const fileToCompress = "src/zip/files/fileToCompress.txt";
+  const fileToSave = "src/zip/files/archive.gz";
+
+  const readableStream = createReadStream(fileToCompress);
+  const writableStream = createWriteStream(fileToSave);
+  const archive = createGzip();
+
+  readableStream.pipe(archive).pipe(writableStream);
 };
+
+compress();
