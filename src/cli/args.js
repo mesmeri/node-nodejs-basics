dfcd67bcca1f0c3de1ce8d4d3a0a49f1ca5aba54
mesmeri <angels_ua@ukr.net>
2022-06-04 19:21:31 +0300

    feat: add logic for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
+import { argv } from "node:process";
+
 export const parseArgs = () => {
-    // Write your code here 
+  const parsedArgs = argv.reduce((acc, val, index, arr) => {
+    if (val.startsWith("--") && arr[index + 1]) {
+      acc.push(`${val.slice(2)} is ${arr[index + 1]}`);
+    }
+    return acc;
+  }, []);
+
+  console.log(parsedArgs.join(", "));
 };
+
+parseArgs();
