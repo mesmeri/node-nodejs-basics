1134d1e54e5879795896481817d5638a98561d57
mesmeri <angels_ua@ukr.net>
2022-06-04 18:23:47 +0300

    feat: add list file names logic

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,26 @@
+import { readdir, access } from "fs/promises";
+
 export const list = async () => {
-    // Write your code here 
+  const filesDirPath = "src/fs/files";
+
+  try {
+    await access(filesDirPath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    const files = await readdir(filesDirPath);
+    const filesNames = [];
+
+    for (const file of files) {
+      filesNames.push(file);
+    }
+
+    console.log(filesNames);
+  } catch {
+    throw new Error("Unexpected error", err);
+  }
 };
+
+list();
