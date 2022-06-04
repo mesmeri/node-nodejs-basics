ccb672d965e2c7f9ae60870366f2af005454ca32
mesmeri <shuptaroks@gmail.com>
2022-06-04 15:03:14 +0300

    feat: add create file logic

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,29 @@
+import { access, writeFile } from "fs/promises";
+import { constants } from "fs";
+
 export const create = async () => {
-    // Write your code here 
+  const filePath = "src/fs/files/fresh.txt";
+  const fileText = "I am fresh and young";
+
+  let fileExists;
+
+  try {
+    await access(filePath, constants.F_OK);
+
+    fileExists = true;
+  } catch {
+    fileExists = false;
+  }
+
+  if (fileExists) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await writeFile(filePath, fileText);
+  } catch (err) {
+    console.error("Unexpected error", err);
+  }
 };
+
+create();
