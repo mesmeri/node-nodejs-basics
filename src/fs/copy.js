e76682a423f5d24666212ac54ae65acab11ee3d8
mesmeri <angels_ua@ukr.net>
2022-06-04 18:22:54 +0300

    feat: add copy files logic

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,44 @@
+import { access, mkdir, copyFile, opendir } from "fs/promises";
+import { constants } from "fs";
+import path from "path";
+
 export const copy = async () => {
-    // Write your code here 
+  const filesPath = "src/fs/files";
+  const filesCopyPath = "src/fs/files_copy";
+
+  let filesCopyFolderExists;
+
+  try {
+    await access(filesPath, constants.R_OK);
+  } catch {
+    throw new Error("FS operation failed ");
+  }
+
+  try {
+    await access(filesCopyPath, constants.R_OK);
+    filesCopyFolderExists = true;
+  } catch {
+    filesCopyFolderExists = false;
+  }
+
+  if (filesCopyFolderExists) {
+    throw new Error("FS operation failed ");
+  }
+
+  try {
+    await mkdir(filesCopyPath);
+
+    const filesDir = await opendir(filesPath);
+
+    for await (const file of filesDir) {
+      copyFile(
+        path.resolve(filesPath, file.name),
+        path.resolve(filesCopyPath, file.name)
+      );
+    }
+  } catch (err) {
+    console.error("Unexpected error", err);
+  }
 };
+
+copy();
